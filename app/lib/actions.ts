66817cc33f5f726cc36d50d3d8f88d320d1199e3
Asad Khan <asadkhan6192@gmail.com>
2024-01-15 23:21:52 +0500

    Add timestamp to messages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { db } from "@/firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { messageSchema } from "./schema";
 
 export async function createMessage(initialState: any, formData: FormData) {
@@ -9,7 +9,7 @@ export async function createMessage(initialState: any, formData: FormData) {
   const parsed = messageSchema.safeParse(data);
   if (parsed.success) {
     const messageRef = collection(db, "Messages");
-    await addDoc(messageRef, parsed.data);
+    await addDoc(messageRef, { ...parsed.data, timestamp: serverTimestamp() });
     return { sucess: true };
   } else {
     return {
